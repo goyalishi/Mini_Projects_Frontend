949649abf7cf3f7ea6b0bd70f7e0953608419116
Ishika Goyal <goyalishika2004@gmail.com>
2024-08-19 02:58:54 +0530

    project added

---
+++ Auto Text Effect Animation/app.js
@@ -0,0 +1,29 @@
+
+let contEle=document.querySelector('.container');
+
+const careers=[' Web Developer','Artist','Engineer','Krishna Bhakt']
+
+let careerIdx=0;
+let charIdx=0;
+
+updateText();
+
+function updateText(){
+    contEle.innerHTML=`<h1>I am 
+    ${(careers[careerIdx].slice(0,1)==="A" || careers[careerIdx].slice(0,1)==="E" )? 'an':'a'} 
+    ${careers[careerIdx].slice(0,charIdx)} <h1>`;
+    charIdx++;
+    if(charIdx===careers[careerIdx].length+1){
+        charIdx=0;
+        careerIdx++;
+    }
+
+    if(careerIdx===careers.length){
+        careerIdx=0;
+    }
+    setTimeout(updateText,400);
+}
+
+
+
+
