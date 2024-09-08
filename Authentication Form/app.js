5c19e5a04cef0191df72a16b5c36d425cfcae504
Ishika Goyal <goyalishika2004@gmail.com>
2024-09-09 00:11:19 +0530

    form added

---
+++ Authentication Form/app.js
@@ -0,0 +1,35 @@
+document.getElementById('submit-btn').addEventListener('click', function(event) {
+    event.preventDefault(); 
+
+    var button = document.getElementById('submit-btn');
+    var spinner = button.querySelector('.spinner');
+    var buttonText = button.querySelector('.button-text');
+    var formContainer = document.querySelector('.form-container');
+    var bgImage = document.getElementById('image');
+    var logo = document.querySelector('.logo');
+
+    
+    buttonText.style.visibility = 'hidden';
+    spinner.style.visibility = 'visible';
+
+    button.classList.add('spinner-active');
+
+    setTimeout(function() {
+        spinner.style.visibility = 'hidden';
+        buttonText.style.visibility = 'visible';
+        button.classList.remove('spinner-active');
+
+        formContainer.style.animation = 'slide-down 2s forwards';
+        
+        setTimeout(function() {
+           
+            bgImage.style.animation = 'shrink-and-disappear 2s forwards';
+            setTimeout(function() {
+              
+                logo.style.visibility = 'visible';
+                logo.style.animation = 'pop-in-center 2s forwards';
+            }, 2000); 
+        }, 2000); 
+
+    }, 2000); 
+});
